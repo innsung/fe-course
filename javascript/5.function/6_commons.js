c766274a3077554fff4fb419a10f3ff176d5f2a7
innsung <vvcluo87@gmail.com>
2026-03-23 18:21:45 +0900

    2026-03-23 commit

---
+++ javascript/5.function/6_commons.js
@@ -0,0 +1,10 @@
+/**
+ * commons 폴더에 저장된 함수 호출
+ */
+
+import { singleGugudan, multiGugudan } from "../commons/gugudan.js";
+
+singleGugudan(3);
+singleGugudan(7);
+multiGugudan(3, 7);
+multiGugudan(1, 2);
