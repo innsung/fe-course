1702e1c267e7e0a34395505c81cb7cfecb710f5d
innsung <vvcluo87@gmail.com>
2026-04-03 18:21:03 +0900

    2026-04-03 commit

---
+++ cgv-demo/js/cgv_join.js
@@ -0,0 +1,62 @@
+/**
+ * DOM 객체 생성 후 객체별 회원가입 이벤트 호출
+ */
+window.addEventListener('DOMContentLoaded', () => {
+    let id = document.querySelector('#id');
+    let idMsg = document.querySelector('#idMsg');
+    let pwd = document.querySelector('#pwd');
+    let pwdMsg = document.querySelector('#pwdMsg');
+    let cpwd = document.querySelector('#cpwd');
+    let cpwdMsg = document.querySelector('#cpwdMsg');
+    let name = document.querySelector('#name');
+    let nameMsg = document.querySelector('#nameMsg');
+
+    // 아이디 체크
+    id.addEventListener('input', () => {
+        if(id.value.trim() !== ''){
+            idMsg.style.display = 'none';
+        } else {
+            idMsg.style.display = 'inline-block';
+        }
+    });
+    // 비밀번호 체크
+    pwd.addEventListener('input', () => {
+        if(pwd.value.trim() !== ''){
+            pwdMsg.style.display = 'none';
+        } else {
+            pwdMsg.style.display = 'inline-block';
+        }
+    });
+    // 비밀번호 확인 체크
+    cpwd.addEventListener('input', () => {
+        if(cpwd.value.trim() !== ''){
+            cpwdMsg.style.display = 'none';
+        } else {
+            cpwdMsg.style.display = 'inline-block';
+        }
+    });
+    //이름 체크
+    name.addEventListener('input', () => {
+        if(name.value.trim() !== ''){
+            nameMsg.style.display = 'none';
+        } else {
+            nameMsg.style.display = 'inline-block';
+        }
+    });
+
+    //가입하기 버튼 이벤트
+    btnSignup.addEventListener('click', () => {
+        let isVaild = true;
+
+        if(id.value.trim() === ''){
+            idMsg.style.dislpay = 'inline-block';
+            isVaild = false;
+        } else {
+            if(isVaild) {
+                console.log('서버 전송 -->', id.value);
+            }
+        }
+    });
+
+
+})  //window event
