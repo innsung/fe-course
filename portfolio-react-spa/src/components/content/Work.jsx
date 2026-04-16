7546825fc598fc62f71d971aebd027d1b2b704ee
innsung <vvcluo87@gmail.com>
2026-04-16 17:57:13 +0900

    2026-04-16 commit

---
+++ portfolio-react-spa/src/components/content/Work.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Title, SubTitle } from '../commons/Titles.jsx';
+import Categories from './Categories.jsx';
+import Projects from './Projects.jsx';
+import { useOutletContext } from 'react-router-dom';
+
+export default function Work() {
+    const data = useOutletContext();
+    return (
+        <section id="work" className="section container">
+            <Title title="My Work" />
+            <SubTitle subTitle="Projects" />
+            <Categories categories={data?.work?.categories} />
+            <Projects projects={data?.work?.projects} />
+        </section>
+    );
+}
+
+
